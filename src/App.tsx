174ef39467c26e34095928bbb4ca8b8efddb1bae
vladnen97 <darkdescent1991@gmail.com>
2023-02-24 16:38:15 +0300

    logic, styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+    const [value, setValue] = useState<number>(0)
 
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className={'counter'}>
+                <h1 className={'display' + (value === 5 ? ' max' : '')}>{value}</h1>
+                <div className={'buttons'}>
+                    <button className={'button' + (value === 5 ? ' disabled' : ' default')} disabled={value === 5} onClick={() => setValue(value + 1)}>inc</button>
+                    <button className={'button' + (value === 0 ? ' disabled' : ' default')} disabled={value === 0} onClick={() => setValue(0)}>reset</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
