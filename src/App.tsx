0dd42d450ac0543a4447071d06262c4c843b76a7
vladnen97 <darkdescent1991@gmail.com>
2023-02-26 18:42:26 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Counter} from './shared/Counter';
 
 function App() {
     const [value, setValue] = useState<number>(0)
 
+    useEffect(() => {
+            const valueFromStorage = localStorage.getItem("counterValue")
+            if (valueFromStorage) {
+                setValue(JSON.parse(valueFromStorage))
+            }
+    }, [])
+    useEffect(() => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+    }, [value])
+
     function incValue(): void {
         setValue(value + 1)
     }
     function resetValue(): void {
-        setValue(0)
-    }
-    function setLocalStorage(): void {
-    localStorage.setItem("counterValue", JSON.stringify(value))
-    }
-    function getLocalStorage(): void {
-        const valueFromStorage = localStorage.getItem("counterValue")
-        if (valueFromStorage) {
-            setValue(JSON.parse(valueFromStorage))
-        }
-    }
-    function clearLocalStorage(): void {
-        localStorage.clear()
         setValue(0)
     }
 
@@ -30,9 +27,7 @@ function App() {
             <Counter value={value}
                      incValue={incValue}
                      resetValue={resetValue}
-                     setLocalStorage={setLocalStorage}
-                     getLocalStorage={getLocalStorage}
-                     clearLocalStorage={clearLocalStorage}/>
+            />
         </div>
     );
 }
