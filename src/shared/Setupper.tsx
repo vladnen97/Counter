c4ac1fcd4605b8746d10507d6701d60144a7c33b
vladnen97 <darkdescent1991@gmail.com>
2023-03-08 07:59:04 +0300

    logic

--- src/shared/Setupper.tsx
+++ src/shared/Setupper.tsx
@@ -1,39 +1,59 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import {Button} from './Button';
-import {SetupType} from '../App';
+import {ModeType, SetupType} from '../App';
 
 type PropsType = {
-
+    setToLocalStorage: (setup: SetupType) => void
+    setMode: (value: ModeType) => void
+    mode: ModeType
+    localSetup: SetupType
 }
 
-export const Setupper = ({}: PropsType) => {
-    const [inputValue, setInputValue] = useState<SetupType>({maxValue: 5, startValue: 0})
+export const Setupper = (props: PropsType) => {
+    const [inputValue, setInputValue] = useState<SetupType>(props.localSetup)
+
+    useEffect(() => {
+        const valuesFromStorage = localStorage.getItem('setup')
+        valuesFromStorage && setInputValue(JSON.parse(valuesFromStorage))
+    }, [])
 
 
     const changeMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
         setInputValue({...inputValue, maxValue: +e.currentTarget.value})
+        props.setMode('edit')
+
     }
     const changeStartValue = (e: ChangeEvent<HTMLInputElement>) => {
         setInputValue({...inputValue, startValue: +e.currentTarget.value});
+        props.setMode('edit')
     }
+
+
+    if (inputValue.maxValue <= inputValue.startValue || inputValue.startValue < 0) {
+        props.setMode('error')
+    }
+
+
+
     return (
         <div className={'counter'}>
             <div className={'display'}>
                 <div className={'wrapper'}>
                     <span>max value:</span>
-                    <input className={'inputClass'} type="number" value={inputValue.maxValue} onChange={changeMaxValue}/>
+                    <input className={'inputClass' + (inputValue.maxValue <= inputValue.startValue ? ' inputError' : '')} type="number" value={inputValue.maxValue}
+                           onChange={changeMaxValue}/>
                 </div>
 
                 <div className={'wrapper'}>
                     <span>start value:</span>
-                    <input className={'inputClass'} type="number" value={inputValue.startValue} onChange={changeStartValue}/>
+                    <input className={inputValue.maxValue <= inputValue.startValue || inputValue.startValue < 0 ? 'inputClass inputError' : 'inputClass'} type="number" value={inputValue.startValue}
+                           onChange={changeStartValue}/>
                 </div>
             </div>
 
             <div className={'buttons'}>
-                <Button className={'button default'}>
-                    set
-                </Button>
+                <Button onClick={() => props.setToLocalStorage(inputValue)}
+                        disabled={props.mode !== 'edit'}>set</Button>
             </div>
         </div>
     );
