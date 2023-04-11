fc14191332ae296e6675cddf689d3e2cf151e5bb
vladnen97 <darkdescent1991@gmail.com>
2023-04-11 10:18:44 +0300

    remove all necessary

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
 import {Counter} from './shared/Counter';
 import {Setupper} from './shared/Setupper';
 
-export type SetupType = {maxValue: number, startValue: number }
-export type ModeType = 'view' | 'edit' | 'error'
 
 function App() {
-    const [localSetup, setLocalSetup] = useState<SetupType>({maxValue: 5, startValue: 0})
-    const [mode ,setMode] = useState<ModeType>('view')
-
-    useEffect(() => {
-        const valuesFromStorage = localStorage.getItem('setup')
-        valuesFromStorage && setLocalSetup(JSON.parse(valuesFromStorage));
-    }, [])
-
-    function setToLocalStorage(setup: SetupType): void {
-        localStorage.setItem('setup', JSON.stringify(setup))
-        setLocalSetup(setup)
-        setMode('view')
-    }
-
 
     return (
         <div className="App">
-
-            {mode !== 'view'
-                ? <Setupper setToLocalStorage={setToLocalStorage} setMode={setMode} mode={mode}/>
-                : <Counter mode={mode} localSetup={localSetup} setMode={setMode}/>
-            }
-
+            <Setupper/>
+            <Counter/>
         </div>
-    );
+    )
 }
 
 export default App;
