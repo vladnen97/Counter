ef8a5d8d5433529586a4f171421c54c2cb5d1438
vladnen97 <darkdescent1991@gmail.com>
2023-04-11 10:20:05 +0300

    use data from context instead localStorage

--- src/shared/Counter.tsx
+++ src/shared/Counter.tsx
@@ -1,29 +1,26 @@
 import React, {useEffect, useState} from 'react';
 import {Button} from './Button';
-import {ModeType, SetupType} from '../App';
+import {useSelector} from 'react-redux';
+import {RootStateType} from '../redux/store';
+import {ModeType} from '../redux/counter-reducer';
 
-type PropsType = {
-    mode: ModeType
-    localSetup: SetupType
-    setMode: (mode: ModeType) => void
-}
 
-export function Counter(props: PropsType) {
-    const [value, setValue] = useState<number>(props.localSetup.startValue)
+
+export function Counter() {
+    const mode       = useSelector<RootStateType, ModeType>(state => state.counter.mode)
+    const startValue = useSelector<RootStateType, number>(state => state.counter.startValue)
+    const maxValue   = useSelector<RootStateType, number>(state => state.counter.maxValue)
+    const [value, setValue] = useState<number>(startValue)
 
     useEffect(() => {
-        setValue(props.localSetup.startValue)
-    }, [props.localSetup])
-
+        setValue(startValue)
+    }, [startValue])
 
     function incValue(): void {
-        (value !== props.localSetup.maxValue) && setValue(value + 1)
+        (value !== maxValue) && setValue(value => value + 1)
     }
     function resetValue(): void {
-        setValue(props.localSetup.startValue)
-    }
-    function changeMode():void {
-        props.setMode('edit')
+        setValue(startValue)
     }
 
 
@@ -31,17 +28,16 @@ export function Counter(props: PropsType) {
         <div className={'counter'}>
             <div className={'display'}>
                 {
-                    props.mode === 'view'
-                        ? <h1 className={value === props.localSetup.maxValue ? 'max' : ''}>{value}</h1>
-                        : props.mode === 'edit'
+                    mode === 'view'
+                        ? <h1 className={value === maxValue ? 'max' : ''}>{value}</h1>
+                        : mode === 'edit'
                             ? <div className={'warning'}>Enter value and press 'set'</div>
                             : <div className={'error'}>Incorrect value!</div>
                 }
             </div>
             <div className={'buttons'}>
-                <Button onClick={incValue} disabled={value === props.localSetup.maxValue}>inc</Button>
-                <Button onClick={resetValue} disabled={value === props.localSetup.startValue}>reset</Button>
-                <Button onClick={changeMode}>set</Button>
+                <Button onClick={incValue} disabled={mode !== 'view' || (value === maxValue)}>inc</Button>
+                <Button onClick={resetValue} disabled={value === startValue || mode !== 'view'}>reset</Button>
             </div>
         </div>
     );
